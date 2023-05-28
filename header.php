d46ed76f8b3ccfd476c1b9de42d0e1d8d345b3fd
Ange Giuntini <ange.giuntini.iut@gmail.com>
2023-05-28 23:51:03 +0200

    modification de l'header - menu déroulant

--- header.php
+++ header.php
@@ -1,20 +1,26 @@
 <!-- Menu en bandeau -->
 <nav>
-		<ul>
-			<li><a href="index.php">Accueil</a></li>
-			<li><a href="affiche_base.php">Affichage de la BDD</a></li>
-			<li><a href="#">Piere C'est un BG</a></li>
-			<?php
-				session_start();
-				// Check if the user is logged in
-				if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-					echo '<li><a href="admin.php">Administration</a></li>';
-					echo '<li><a href="#">Vous Etes connectés</a></li>';
-					echo "<li><a href='#'>{$_SESSION['user']}</a></li>";
-				}
-				else {
-					echo '<li class="left"><a href="sign-in.php">Se connecter</a></li>';
-				} 
-			?>
-		</ul>
+	<ul>
+		<li><a href="index.php">Accueil</a></li>
+		<li><a href="affiche_base.php">Affichage de la BDD</a></li>
+		<li><a href="#">Piere C'est un BG</a></li>
+		<?php
+		session_start();
+		// Check if the user is logged in
+		if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+			// Other menu items
+			echo '<li class="allign_right"><a href="admin.php">Administration</a></li>';
+			echo '
+			<li class="dropdown" >
+			  <a href="#" class="dropbtn">' . $_SESSION['user'] . '</a>
+			  <ul class="dropdown-content">
+				<li><a href="logout.php">Déconnexion</a></li>
+			  </ul>
+			</li>';
+		} else {
+			echo '<li class="allign_right"><a href="sign-in.php">Se connecter</a></li>';
+		}
+		?>
+	</ul>
 </nav>
+<script src="./scripts/rolldown.js"></script>
