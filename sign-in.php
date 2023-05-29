3c75f52480fab09f3410c138611d348717fabfb1
Ange Giuntini <131355152+ange-projects@users.noreply.github.com>
2023-05-29 22:04:51 +0200

    Ajout de style - Glass morphisme sign-in.php

--- sign-in.php
+++ sign-in.php
@@ -7,19 +7,21 @@
     <link rel="stylesheet" href="styles/Sign-in.css">
     <title>Se Connecter</title>
 </head>
-    </nav>
+<body>
     <?php require_once 'header.php' ?>
-    <div class="form-title">
-        <h2>Vous êtes de retour !!!</h2>
-        <p>Veuillez vous connecter</p>
-    </div>
-    <form class="register" action="sign-in_check.php" method="post">
-        <label for="user">Utilisateur :</label>
-        <input type="user" id="user" name="user" required>
+    <main><
+        <form class="register" action="sign-in_check.php" method="post">
+            <div class="form-title">
+                <h2>Connectez Vous</h2>
+            </div>
+            <label for="user">Utilisateur :</label>
+            <input type="user" id="user" name="user" required>
 
-        <label for="password">Mot de passe :</label>
-        <input type="password" id="password" name="password" required>
+            <label for="password">Mot de passe :</label>
+            <input type="password" id="password" name="password" required>
 
-        <input class="Round_button" type="submit" value="Se connecter">
-    </form>
+            <input class="Round_button" type="submit" value="Se connecter">
+        </form>
+    </main>
+</body>
 </html>
