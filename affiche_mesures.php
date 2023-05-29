826bbca5c92afaa836cf1a81d84366e6d0970ac4
Ange Giuntini <ange.giuntini.iut@gmail.com>
2023-05-29 14:27:44 +0200

    Affichage des 10 dernières mesures - tri par salles

--- affiche_mesures.php
+++ affiche_mesures.php
@@ -8,49 +8,32 @@
     <title>Document</title>
 </head>
 <body>
-<?php require_once 'header.php' ?>
 
-<table>
-    <tr>
-      <th>ID</th>
-      <th>Date</th>
-      <th>Valeur</th>
-      <th>ID_capteur</th>
-    </tr>
-<?php
-// require the script used to connect to the DB
-require_once('connexion_bdd.php');
 
-// retrieve the mesure table content
-$request_content = mysqli_query($connexion, "SELECT * FROM mesure");
-if (!$request_content) {
-    die("Error : can't retrieve data from mesure" . mysqli_error($connexion));
-}
+    <?php 
+        require_once 'header.php';
+        require_once 'Display_one_sensor.php'; 
+        // Require the script used to connect to the DB
+        require_once('connexion_bdd.php');
 
-// Count the number of lines
-$LineCount = mysqli_num_rows($request_content);
+        // display the last 10 measurements for the room B203
+        echo "<h1>Salle B203</h1>";
+        // Call the function to display data for a specific sensor
+        Display_one("24e124128c011778", "Humidité");
 
-if ($LineCount) {
+        echo "<h1>Salle E102</h1>";
+        Display_one("24e124128c016122", "Humidité");
 
-    while ($line = mysqli_fetch_assoc($request_content)) {
+        echo "<h1>Salle B001</h1>";
+        Display_one("24e124128c012259", "CO2");
 
-        // Affichage des valeurs des lignes
-        echo "<tr>";
-        echo "<td>";
-        echo implode("</td><td>", $line);
-        echo "</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-} else {
-    echo "Pas de ligne";
-}
+        echo "<h1>Salle E006</h1>";
+        Display_one("24e124128c016509", "CO2");
+    ?>
 
-echo "<br>";
-// Fermeture de la connexion à la base de données
-mysqli_close($connexion);
+<?php // Close the database connection
+        mysqli_close($connexion);
 ?>
-
 
 </body>
 </html>
