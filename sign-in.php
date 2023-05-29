7716020e0bafe3af239ca868ebb3c1bdbd8e1d45
Ange Giuntini <ange.giuntini.iut@gmail.com>
2023-05-29 17:52:09 +0200

    Styliser la page de connexion

--- sign-in.php
+++ sign-in.php
@@ -4,22 +4,22 @@
     <meta charset="UTF-8">
     <link href="./Ange/style/style.css" rel="Stylesheet" type="text/css" /> 
     <script src="./scripts/script.js"></script>
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/Sign-in.css">
     <title>Se Connecter</title>
 </head>
     </nav>
     <?php require_once 'header.php' ?>
     <div class="form-title">
-        <h2>Bienvenue</h2>
+        <h2>Vous êtes de retour !!!</h2>
         <p>Veuillez vous connecter</p>
     </div>
     <form class="register" action="sign-in_check.php" method="post">
-        <label for="user">User:</label>
+        <label for="user">Utilisateur :</label>
         <input type="user" id="user" name="user" required>
 
-        <label for="password">Password:</label>
+        <label for="password">Mot de passe :</label>
         <input type="password" id="password" name="password" required>
 
-        <input class="Round_button" type="submit" value="Register">
+        <input class="Round_button" type="submit" value="Se connecter">
     </form>
 </html>
