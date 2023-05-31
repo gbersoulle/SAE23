3dc6294d196c8a79f257c1a06fe90ee26bc3e7b9
Gaspard <76206763+gbersoulle@users.noreply.github.com>
2023-05-31 14:05:54 +0200

    Set up des protection contres les données SQL

--- script_admin.php
+++ script_admin.php
@@ -8,79 +8,154 @@ if(isset($_POST['submit_ajouter_capteur'])){
         exit;
     }
     require('connexion_bdd.php'); 
-    // Récup des valeurs et les met dans la BDD
-    $id_capteur = $_POST['id_capteur'];
-    $nomCapteur = $_POST['nom_capteur'];
-    $typeCapteur = $_POST['type_capteur'];
-    $Salle = $_POST['Salle'];
-    $idBatiment = $_POST['nom_bat'];
-    $sql = "INSERT INTO capteur (id_capteur,nom_capteur, type_capteur, Salle, id_batiment) VALUES ('$id_capteur','$nomCapteur', '$typeCapteur', '$Salle', $idBatiment)"; 
-    echo "$sql <br/>";
-    if (mysqli_query($connexion, $sql)) {
-        echo "Le texte a été ajouté avec succès.";
-    } else {
-        echo "Erreur: " . mysqli_error($connexion);
+    // Récup des valeurs et échappes
+    $id_capteur = mysqli_real_escape_string($connexion,htmlspecialchars($_POST['id_capteur'], ENT_QUOTES, 'UTF-8'));
+    $nomCapteur = mysqli_real_escape_string($connexion,htmlspecialchars($_POST['nom_capteur'], ENT_QUOTES, 'UTF-8'));
+    $typeCapteur = mysqli_real_escape_string($connexion,htmlspecialchars($_POST['type_capteur'], ENT_QUOTES, 'UTF-8'));
+    $Salle = mysqli_real_escape_string($connexion,htmlspecialchars($_POST['salle_capteur'], ENT_QUOTES, 'UTF-8'));
+    $idBatiment = intval($_POST['nom_bat']);
+    $sql = "INSERT INTO capteur (id_capteur, nom_capteur, type_capteur, Salle, id_batiment) VALUES (?, ?, ?, ?, ?)"; 
+    $stmt = mysqli_prepare($connexion, $sql); // permet de préparer la requete dans $stmt 
+
+    // Vérification de la préparation de la requête
+    if ($stmt === false) {
+        echo "Erreur dans la prep de la requete: " . mysqli_error($connexion);
+        exit;
     }
+    mysqli_stmt_bind_param($stmt, "ssssi", $id_capteur, $nomCapteur, $typeCapteur, $Salle, $idBatiment); // s = string (chaine de caractères) i = integrer
+    
+    if (!mysqli_stmt_execute($stmt)) {
+        echo "Erreur: " . mysqli_stmt_error($stmt);
+    }
+    
+    // Fermeture du statement et de la connexion à la base de données
+    mysqli_stmt_close($stmt);
     mysqli_close($connexion);
     
 }
 
-if(isset($_POST['submit_ajouter_battiment'])){
-    if (empty($_POST['login_gest']) || empty($_POST['mdp_gest']) || empty($_POST['nom_bat'])) { //Teste si les champs sont vides pour éviter les capteurs vides
+if (isset($_POST['submit_ajouter_battiment'])) {
+    if (empty($_POST['login_gest']) || empty($_POST['mdp_gest']) || empty($_POST['nom_bat'])) {
         echo "Il y a un soucis au niveau des champs renseignés";
-        exit; 
+        exit;
     }
     require('connexion_bdd.php');
-    // Récup des valeurs et les met dans la BDD
-    $nom_bat = $_POST['nom_bat'];
-    $login_gest = $_POST['login_gest'];
+    
+    // Récup les valeurs et évite les caractères spéciaux (eviter les injections)
+    $nom_bat = mysqli_real_escape_string($connexion, htmlspecialchars($_POST['nom_bat'], ENT_QUOTES, 'UTF-8'));
+    $login_gest = mysqli_real_escape_string($connexion, htmlspecialchars($_POST['login_gest'], ENT_QUOTES, 'UTF-8'));
     $mdp_gest = hash('sha256', $_POST['mdp_gest']);
-    $sql = "INSERT INTO batiment (nom_bat, login_gest, mdp_gest) VALUES ('$nom_bat', '$login_gest', '$mdp_gest')";
-    mysqli_query($connexion, $sql);  
-    mysqli_close($connexion); 
+    $sql = "INSERT INTO batiment (nom_bat, login_gest, mdp_gest) VALUES (?, ?, ?)";
+    $stmt = mysqli_prepare($connexion, $sql);
+    
+    if ($stmt === false) {
+        echo "Erreur dans la préparation de la requête: " . mysqli_error($connexion);
+        exit;
+    }
+    
+    mysqli_stmt_bind_param($stmt, "sss", $nom_bat, $login_gest, $mdp_gest); // lie la requete avec les variables
+    
+    if (!mysqli_stmt_execute($stmt)) {
+        echo "Erreur: " . mysqli_stmt_error($stmt); // execute et  renvoie les erreurs si il y en a 
+    }
+    
+    mysqli_stmt_close($stmt);
+    mysqli_close($connexion);
 }
 
 
+
+
 if (isset($_POST['submit_supprimer_capteur']) && isset($_POST['capteurs'])) {
-    if (empty($_POST['capteurs'])) { //Teste si les champs sont vides pour éviter les capteurs vides
+    if (empty($_POST['capteurs'])) {
         echo "Il y a un soucis au niveau des champs renseignés";
         exit();
     }
     require('connexion_bdd.php');
-    foreach ($_POST['capteurs'] as $idCapteur) {
-        //Pour supprimer capteurs faut avoir suppr toutes les données associées
 
-        $sqlMesureSuppr = "DELETE FROM mesure WHERE id_capteur = " . intval($idCapteur);
-        $sqlCapteurSuppr = "DELETE FROM capteur WHERE id_capteur = " . intval($idCapteur);
+    $sqlMesureSuppr = "DELETE FROM mesure WHERE id_capteur = ?"; // Requetes préparées
+    $sqlCapteurSuppr = "DELETE FROM capteur WHERE id_capteur = ?";
 
-        mysqli_query($connexion, $sqlCapteurSuppr);
-        mysqli_query($connexion, $sqlMesureSuppr);
+    $stmtMesureSuppr = mysqli_prepare($connexion, $sqlMesureSuppr); // Les statements qui vont avec la connxion
+    $stmtCapteurSuppr = mysqli_prepare($connexion, $sqlCapteurSuppr);
+
+    if ($stmtMesureSuppr === false || $stmtCapteurSuppr === false) {
+        echo "Erreur dans la préparation des requêtes : " . mysqli_error($connexion); //Test si erreur de prep
+        exit();
     }
-    mysqli_close($connexion);
 
+    mysqli_stmt_bind_param($stmtMesureSuppr, "i", $idCapteur); //bind de parametres
+    mysqli_stmt_bind_param($stmtCapteurSuppr, "i", $idCapteur);
+
+    foreach ($_POST['capteurs'] as $idCapteur) { //execute pour chaque capteurs
+        if (!mysqli_stmt_execute($stmtMesureSuppr)) {
+            echo "Erreur: " . mysqli_stmt_error($stmtMesureSuppr); // execute et  renvoie les erreurs si il y en a 
+        }
+        if (!mysqli_stmt_execute($stmtCapteurSuppr)) {
+            echo "Erreur: " . mysqli_stmt_error($stmtCapteurSuppr); // execute et  renvoie les erreurs si il y en a 
+        }
+        
+        // mysqli_stmt_execute($stmtMesureSuppr);
+        // mysqli_stmt_execute($stmtCapteurSuppr);
+        
+    }
+
+    // Fermeture des statements et de la connexion à la base de données
+    mysqli_stmt_close($stmtMesureSuppr);
+    mysqli_stmt_close($stmtCapteurSuppr);
+    mysqli_close($connexion);
 }
+
 
 
 
 if (isset($_POST['submit_supprimer_batt']) && isset($_POST['batiment'])) {
-    if (empty($_POST['batiment'])) { //Teste si les champs sont vides pour éviter les capteurs vides
+    if (empty($_POST['batiment'])) {
         echo "Il y a un soucis au niveau des champs renseignés";
         exit();
     }
     require('connexion_bdd.php');
-    foreach ($_POST['batiment'] as $id_batiment) {
-            //Pour supprimer un batt faut avoir supprimé tout les capteurs et toutes les données associées
 
-            $sqlMesureSuppr = "DELETE FROM mesure WHERE id_capteur IN (SELECT id_capteur FROM capteur WHERE id_batiment = $id_batiment)";
-            $sqlCapteurSuppr = "DELETE FROM capteur WHERE id_batiment = $id_batiment";
-            $sqlBattimentSuppr = "DELETE FROM batiment WHERE id_batiment = $id_batiment";
+    $sqlMesureSuppr = "DELETE FROM mesure WHERE id_capteur IN (SELECT id_capteur FROM capteur WHERE id_batiment = ?)";
+    $sqlCapteurSuppr = "DELETE FROM capteur WHERE id_batiment = ?";
+    $sqlBattimentSuppr = "DELETE FROM batiment WHERE id_batiment = ?"; //requetes préparés
 
-            mysqli_query($connexion, $sqlMesureSuppr);
-            mysqli_query($connexion, $sqlCapteurSuppr);
-            mysqli_query($connexion, $sqlBattimentSuppr);
+    $stmtMesureSuppr = mysqli_prepare($connexion, $sqlMesureSuppr);
+    $stmtCapteurSuppr = mysqli_prepare($connexion, $sqlCapteurSuppr);
+    $stmtBattimentSuppr = mysqli_prepare($connexion, $sqlBattimentSuppr); //prparation des statements
+
+    if ($stmtMesureSuppr === false || $stmtCapteurSuppr === false || $stmtBattimentSuppr === false) {
+        echo "Erreur dans la préparation des requêtes : " . mysqli_error($connexion); //test d'erreur
+        exit();
     }
-    mysqli_close($connexion);
+
+    mysqli_stmt_bind_param($stmtMesureSuppr, "i", $id_batiment);
+    mysqli_stmt_bind_param($stmtCapteurSuppr, "i", $id_batiment);
+    mysqli_stmt_bind_param($stmtBattimentSuppr, "i", $id_batiment); // Liaison des paramètres des statements
+
+    foreach ($_POST['batiment'] as $id_batiment) { //execution pour chaque batiments
+
+        if (!mysqli_stmt_execute($stmtMesureSuppr)) {
+            echo "Erreur: " . mysqli_stmt_error($stmtMesureSuppr); // execute et  renvoie les erreurs si il y en a  pour les meusures
+        }
+        if (!mysqli_stmt_execute($stmtCapteurSuppr)) {
+            echo "Erreur: " . mysqli_stmt_error($stmtCapteurSuppr); // execute et  renvoie les erreurs si il y en a pour les capteurs
+        }
+        if (!mysqli_stmt_execute($stmtBattimentSuppr)) {
+            echo "Erreur: " . mysqli_stmt_error($stmtBattimentSuppr); // execute et  renvoie les erreurs si il y en a pour les battimetns
+        }
+
+        // mysqli_stmt_execute($stmtMesureSuppr);
+        // mysqli_stmt_execute($stmtCapteurSuppr);
+        // mysqli_stmt_execute($stmtBattimentSuppr);
+    }
+
+    mysqli_stmt_close($stmtMesureSuppr);
+    mysqli_stmt_close($stmtCapteurSuppr);
+    mysqli_stmt_close($stmtBattimentSuppr);
+    mysqli_close($connexion); //fermeture des statement et de la connexion mysql
 }
+
 
 
 
