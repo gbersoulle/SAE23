1ae7a1df1608e4692dd97c6ec7d6ec79aade8cd9
Gaspard <76206763+gbersoulle@users.noreply.github.com>
2023-06-06 11:39:17 +0200

    On peut maintenant se connecter en tant que gestionnaire dans le site

--- header.php
+++ header.php
@@ -4,13 +4,20 @@
 		<li><a href="index.php">Accueil</a></li>
 		<li><a href="affiche_base.php">Affichage de la BDD</a></li> <!-- ///////////////à enlever à la fin//////////////// -->
 		<li><a href="consultation.php">Consultation</a></li>
-		<li><a href="gestion.php">Gestion (temporaire)</a></li>
 		<?php
 		session_start();
 		// Check if the user is logged in
 		if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-			// Other menu items
-			echo '<li class="allign_right"><a href="admin.php">Administration</a></li>';
+			// Check the user source
+			if (isset($_SESSION['source']) && $_SESSION['source'] == 'administration') {
+				// User is from administration table, show link to admin.php
+				echo '<li class="allign_right"><a href="admin.php">Administration</a></li>';
+			} elseif (isset($_SESSION['source']) && $_SESSION['source'] == 'batiment') {
+				// User is from batiment table, show link to gestion.php
+				echo '<li class="allign_right"><a href="gestion.php">Gestion</a></li>';
+			}
+
+			// Show user dropdown menu
 			echo '
 			<li class="dropdown" >
 			  <a href="#" class="dropbtn">' . $_SESSION['user'] . '</a>
@@ -22,7 +29,7 @@
 			echo '<li class="allign_right"><a href="sign-in.php">Se connecter</a></li>';
 		}
 		?>
-		<!-- differentier quelqu'un qui s'es log en tant qu'admin ou en tant que gestionnaire et si gestionnaire donner acces à admin.php si admin, donner acce a gestion.php -->
 	</ul>
 </nav>
 <script src="./scripts/rolldown.js"></script>
+
--- sign-in_check.php
+++ sign-in_check.php
@@ -1,5 +1,4 @@
 <?php
-
 // Start the session
 session_start();
 
@@ -13,15 +12,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $password = $_POST['password'];
 
   // Query the database for the user with the given username and password
-  $sql = "SELECT * FROM administration WHERE user = '$user' AND password = '$password'";
+  $sql = "(SELECT 'administration' AS source, user, password FROM administration WHERE user = '$user' AND password = '$password')
+          UNION
+          (SELECT 'batiment' AS source, login_gest AS user, mdp_gest AS password FROM batiment WHERE login_gest = '$user' AND mdp_gest = '$password')";
   $result = mysqli_query($connexion, $sql);
 
   // Check if a user was found
   if (mysqli_num_rows($result) == 1) {
+    // Fetch the user information from the result
+    $row = mysqli_fetch_assoc($result);
+    $source = $row['source']; //correspond à la table source (l'utilisateur est dans la table administration ou dans la table batiment (gesitonnaire donc))
+    $user = $row['user'];
+
     // Set the session variable to indicate that the user is logged in
     $_SESSION['loggedin'] = true;
     $_SESSION['user'] = $user;
-
+    $_SESSION['source'] = $source;
 
     // Redirect the user to the home page
     header('Location: index.php');
