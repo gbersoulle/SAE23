2b4573c70909550587d4f616fe6849a530dbba84
Gaspard <76206763+gbersoulle@users.noreply.github.com>
2023-06-06 15:29:23 +0200

    Mise en place d'un filtre pour visualiser les capteurs

--- gestion.php
+++ gestion.php
@@ -18,13 +18,42 @@
     $requeteBatiment = "SELECT id_batiment FROM batiment WHERE login_gest = '$nomUtilisateur'";
     $resultatBatiment = mysqli_query($connexion, $requeteBatiment);
     $ligneBatiment = mysqli_fetch_assoc($resultatBatiment);
-    $idBatiment = $ligneBatiment['id_batiment']; //Récupère le batiment géré par cet utilisateur
+    $idBatiment = $ligneBatiment['id_batiment']; //Récupère le bâtiment géré par cet utilisateur
 
     $requeteCapteurs = "SELECT nom_capteur, type_capteur FROM capteur WHERE id_batiment = '$idBatiment'";
-    $resultatCapteurs = mysqli_query($connexion, $requeteCapteurs); //récup tout les capteurs du bat en question
+    $resultatCapteurs = mysqli_query($connexion, $requeteCapteurs); //récupère tous les capteurs du bâtiment en question
+    ?>
+    <form method='GET'>
+    <label for='capteur'>Choisir un capteur :</label>
+    <select name='capteur' id='capteur'>
+    <option value=''>Tous les capteurs</option>
+        <?php
+        // Ajout des options pour chaque capteur
+        while ($ligneCapteur = mysqli_fetch_assoc($resultatCapteurs)) {
+            $nomCapteur = $ligneCapteur['nom_capteur'];
+            echo "<option value='$nomCapteur'>$nomCapteur</option>";
+        }
+        ?>
+    </select>
+    <input type='submit' value='Filtrer'>
+    </form>
 
-    // pour chaque capteur, en fonction de son type, avoir l'unitée, et en faire un tableau
-    while ($ligneCapteur = mysqli_fetch_assoc($resultatCapteurs)) {
+
+
+    <?php
+    $capteurSelectionne = isset($_GET['capteur']) ? $_GET['capteur'] : ''; //capteur sélectionné
+
+    // Requête SQL pour obtenir les capteurs en fonction du filtre
+    $requeteCapteursFiltre = "SELECT nom_capteur, type_capteur FROM capteur WHERE id_batiment = '$idBatiment'";
+
+    if (!empty($capteurSelectionne)) {
+        $requeteCapteursFiltre .= " AND nom_capteur = '$capteurSelectionne'";
+    }
+
+    $resultatCapteursFiltre = mysqli_query($connexion, $requeteCapteursFiltre);
+
+    // pour chaque capteur, en fonction de son type, avoir l'unité et en faire un tableau
+    while ($ligneCapteur = mysqli_fetch_assoc($resultatCapteursFiltre)) {//fait un tableau pour chaque capteur sélectionné dans la requete SQL (qui font parti du filtre)
         $nomCapteur = $ligneCapteur['nom_capteur'];
         $type_capteur = $ligneCapteur['type_capteur'];
 
@@ -61,19 +90,19 @@
                 $unite = "";
         }
 
-       // Obtenir la moyenne pour le capteur
+        // Obtenir la moyenne pour le capteur
         $requeteMoyCapteur = "SELECT AVG(valeur_mesure) AS moyenne FROM mesure WHERE nom_capteur = '$nomCapteur'";
         $resultatMoyCapteur = mysqli_query($connexion, $requeteMoyCapteur);
         $ligneMoyCapteur = mysqli_fetch_assoc($resultatMoyCapteur);
         $moyenneCapteur = $ligneMoyCapteur['moyenne'];
 
-        echo "<h2>Moyenne du capteur $nomCapteur : $moyenneCapteur $unite</h2>";
+        echo "<h2>Moyenne du capteur de $type_capteur : $nomCapteur : $moyenneCapteur $unite</h2>";
 
-        // Recup et afficher Les valeurs historiques
-        $requeteHistorique = "SELECT * FROM mesure WHERE nom_capteur = '$nomCapteur'";
-        $resultatHistorique = mysqli_query($connexion, $requeteHistorique);
+        // Récupérer et afficher les valeurs historiques
+        $requeteValHistorique = "SELECT * FROM mesure WHERE nom_capteur = '$nomCapteur'";
+        $resultatValHistorique = mysqli_query($connexion, $requeteValHistorique);
 
-        if (mysqli_num_rows($resultatHistorique) > 0) {
+        if (mysqli_num_rows($resultatValHistorique) > 0) {
             echo "<table>
                     <tr>
                         <th>ID Mesure</th>
@@ -81,11 +110,11 @@
                         <th>Valeur Mesure</th>
                         <th>Nom Capteur</th>
                     </tr>";
-            while ($ligneHistorique = mysqli_fetch_assoc($resultatHistorique)) {
-                $idMesure = $ligneHistorique['id_mesure'];
-                $dateMesure = $ligneHistorique['date_mesure'];
-                $valeurMesure = $ligneHistorique['valeur_mesure'];
-                $nomCapteur = $ligneHistorique['nom_capteur'];
+            while ($ligneValHistorique = mysqli_fetch_assoc($resultatValHistorique)) {
+                $idMesure = $ligneValHistorique['id_mesure'];
+                $dateMesure = $ligneValHistorique['date_mesure'];
+                $valeurMesure = $ligneValHistorique['valeur_mesure'];
+                $nomCapteur = $ligneValHistorique['nom_capteur'];
                 echo "<tr>
                         <td>$idMesure</td>
                         <td>$dateMesure</td>
