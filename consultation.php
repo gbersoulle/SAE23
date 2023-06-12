cf8d95a65f2748bd5e97932a94472be66a1a381c
Ange Giuntini <ange.giuntini.iut@gmail.com>
2023-06-12 11:15:22 +0200

    métriques par type
    moyenne de chaque dernière valeur par type dans consultation.php

--- consultation.php
+++ consultation.php
@@ -18,23 +18,6 @@
 					// Call a function to display data for "all" buildings, display "1" measurment
                     display_all_buildings("all", 1, '', '', '', '', '', '');
 
-                    //display metrics (avg, min, max)
-                    echo "<h1>Métriques par Salles</h1>";
-                    echo "
-                    <table class = 'metriques'>
-                        <tr>
-                            <th>Salle</th>
-                            <th>Type de Donnée</th>
-                            <th>Moyenne</th>
-                            <th>Minimum</th>
-                            <th>Maximum</th>
-                        </tr>";
-                      $moyenne_h[] = Display_moyenne("B203","AM107-6","Humidité");
-                      $moyenne_h[] = Display_moyenne("E102","AM107-32","Humidité");
-                      $moyenne_c[] = Display_moyenne("B001","AM107-7","CO2");
-                      $moyenne_c[] = Display_moyenne("E006","AM107-29","CO2");
-                    echo "</table>";
-                
                     echo "<h1>Métriques par Type de données</h1>";
                     echo "
                     <table class = 'metriques'>
@@ -44,8 +27,7 @@
                             <th>Minimum</th>
                             <th>Maximum</th>
                         </tr>";
-                      $mptd[] = Metrique_type($moyenne_h, "Humidité");
-                      $mptd[] = Metrique_type($moyenne_c, "CO2");
+                        Display_moyenne();
                     echo "</table>";
                     ?>
             </div>
